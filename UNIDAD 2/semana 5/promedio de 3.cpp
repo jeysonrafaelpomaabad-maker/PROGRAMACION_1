c4ba83b77afbf8f184d4dc43424d61f6735c43ca
jeysonrafaelpomaabad-maker <jetsonrafaelpomaabad@gmail.com>
2026-07-17 09:17:30 -0500

    Add files via upload

---
+++ UNIDAD 2/semana 5/promedio de 3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+#include<time.h>
+
+using namespace std; 
+int main(){
+  
+  cout << "los numeros impares son :"<< endl;
+  
+  for (int i = 1; i <= 40; i=i + 2)
+  {
+   cout << "numero :"<< i <<endl;
+  }
+	
+
+	}
+	
